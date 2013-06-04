5f7d0af607208b415d39136007415791fea48e8c
Schdegga <wenninger.s@gmx.de>
2013-06-04 12:31:59 +0200

    Receive Messages from other Users while in ChatRoom
    +
    Code Refactoring
    Comments

--- src/com/example/chatapp/MainChatActivity.java
+++ src/com/example/chatapp/MainChatActivity.java
@@ -11,6 +11,7 @@ import org.jivesoftware.smack.Roster;
 import org.jivesoftware.smack.RosterEntry;
 import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.filter.PacketFilter;
 import org.jivesoftware.smack.filter.PacketTypeFilter;
 import org.jivesoftware.smack.packet.Message;
 import org.jivesoftware.smack.packet.Packet;
@@ -61,8 +62,12 @@ public class MainChatActivity extends Activity {
 	private ChatManager    chatManager;
 	private static Context context;
 	private ArrayAdapter<ChatPartner> adapter;
+	private String currentChatPartner;
 
-
+	
+	/**
+	 * onCreate Android Lifecycle Method
+	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -76,30 +81,10 @@ public class MainChatActivity extends Activity {
 		addButton = (Button) findViewById(R.id.addChatPartner);
 		listView = (ListView) findViewById(R.id.chatPartners);
 		context = this;
+		currentChatPartner = "";
 		
-		
-		// Handle subscriptions
-		connection.addPacketListener(new PacketListener() {
-			@Override
-			public void processPacket(Packet packet) {
-				handlePresencePackets(packet);
-			}
-		}, new PacketTypeFilter(Presence.class));
-		
-		
-		/*
-		 *
-		// Handle Messages that come outside of ChatRoom
-		connection.addPacketListener(new PacketListener() {
-			@Override
-			public void processPacket(Packet packet) {
-				handleMessagePackets(packet);
-			}
-		}, new PacketTypeFilter(Message.class));
-		*
-		*
-		*/
-		
+		// Set Listeners
+		initialiseListeners();
 		
 		// Get all ChatPartners and load them into Array
 		Collection<RosterEntry> rosterEntries = chatPartners.getEntries();
@@ -111,53 +96,142 @@ public class MainChatActivity extends Activity {
 		// Set ListView - Adapter
 		adapter = new ArrayAdapter<ChatPartner>(this, R.layout.chat_partner_view, R.id.chat_partner_item_view, chats);
 		listView.setAdapter(adapter);
-		
-		// Handle ListItem Clicks
-		listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-			@Override
-			public boolean onItemLongClick(AdapterView<?> parent, View view,
-					int position, long id) {
-				
-				ChatPartner clickedUser = (ChatPartner) parent.getItemAtPosition(position);
-				return removeUser(clickedUser);
-			}
-		});
-		
-		listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-			@Override
-			public void onItemClick(AdapterView<?> parent, View view, int position,
-					long id) {
-				
-				// Get Clicked User, and start Chatroom
-				ChatPartner clickedUser = (ChatPartner) parent.getItemAtPosition(position);
-				Intent intent = new Intent(view.getContext(), ChatRoomActivity.class);
-				intent.putExtra("chosenChatPartnerEmail", clickedUser.getEmail());
-				intent.putExtra("chosenChatPartnerName", clickedUser.getName());
-				intent.putStringArrayListExtra("messagesOutsideChatroom", clickedUser.getMessages());
-				startActivityForResult(intent, CHAT_ROOM_REQUEST);
-			}
-		});
-		
-		// Let User choose account from his phonebook
-		addButton.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				Intent intent = new Intent(v.getContext(), ChooseAccountActivity.class);
-				intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-				startActivityForResult(intent, PICK_ACCOUNT_REQUEST);
-			}
-		});
 	}
 	
 	
+	
+	/**
+	 * Android Method
+	 */
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main_chat, menu);
+		return true;
+	}
+	
+	
+	
+	/**
+	 * Method for keeping track of the current Activity we are in. Is called by other Activites.
+	 * 
+	 * @param newContext is the Activity we are currently in. Is used to correctly display Toasts and Dialogs
+	 *  				 even though the Packet and Message Listeners are initialised here, and not in ChatRoom- or ChooseAccountAcitivty
+	 */
 	public static void setContext(Context newContext)
 	{
 		context = newContext;
 	}
 	
 	
+	
+	/**
+	 * Sets all Listeners
+	 */
+	private void initialiseListeners() 
+	{
+		// Handle subscriptions
+		connection.addPacketListener(new PacketListener() 
+		{
+			@Override
+			public void processPacket(Packet packet) 
+			{
+				handlePresencePackets(packet);
+			}
+		}, new PacketTypeFilter(Presence.class));
+		
+		// Handle Messages that come outside of ChatRoom
+		connection.addPacketListener(new PacketListener() 
+		{
+			@Override
+			public void processPacket(Packet packet) 
+			{
+				handleMessagePackets(packet);
+			}
+		}, new MyPacketFilter());
+		
+		// Handle ListItem Clicks
+		// Long Clicks
+		listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() 
+		{
+			@Override
+			public boolean onItemLongClick(AdapterView<?> parent, View view,
+					int position, long id) 
+			{
+				ChatPartner clickedUser = (ChatPartner) parent.getItemAtPosition(position);
+				return handleLongListViewClick(view.getContext(), clickedUser);
+			}
+		});
+		
+		// Short Clicks
+		listView.setOnItemClickListener(new AdapterView.OnItemClickListener() 
+		{
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position,
+					long id) 
+			{
+				// Get Clicked User, and start Chatroom
+				ChatPartner clickedUser = (ChatPartner) parent.getItemAtPosition(position);
+				handleShortListViewClick(view.getContext(), clickedUser);
+			}
+		});
+		
+		// Let User choose account from his phonebook
+		addButton.setOnClickListener(new View.OnClickListener() 
+		{
+			@Override
+			public void onClick(View v) 
+			{
+				Intent intent = new Intent(v.getContext(), ChooseAccountActivity.class);
+				intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+				startActivityForResult(intent, PICK_ACCOUNT_REQUEST);
+			}
+		});
+	}
+
+	
+	
+	/**
+	 * Goes into ChatRoom with selected User
+	 * 
+	 * @param viewContext is the Context, the ListView was clicked in
+	 * @param clickedUser is the User we want to chat with
+	 */
+	protected void handleShortListViewClick(Context viewContext, ChatPartner clickedUser) 
+	{
+		Intent intent = new Intent(viewContext, ChatRoomActivity.class);
+		intent.putExtra("chosenChatPartnerEmail", clickedUser.getEmail());
+		intent.putExtra("chosenChatPartnerName", clickedUser.getName());
+		intent.putStringArrayListExtra("messagesOutsideChatroom", clickedUser.getMessages());
+		
+		// Remember that we are in a ChatRoom with somebody
+		currentChatPartner = clickedUser.getEmail();
+		
+		startActivityForResult(intent, CHAT_ROOM_REQUEST);
+	}
+
+	
+
+	/**
+	 * Deletes User on long Item Click in ListView
+	 * 
+	 * @param viewContext is the Context, the ListView was clicked in
+	 * @param clickedUser is the Item that was clicked
+	 * @return
+	 */
+	protected boolean handleLongListViewClick(Context viewContext, ChatPartner clickedUser) 
+	{
+		//TODO Let User decide if he really wants to delete ChatPartner!
+		return removeUser(clickedUser);
+	}
+
+
+	
+	/**
+	 * Stores received Messages that come from Users, we currently are NOT in a ChatRoom with
+	 * 
+	 * @param packet is the Message Packet that was sent to us
+	 */
 	private void handleMessagePackets(Packet packet) {
 		
 		Log.i("MESSAGE HANDLER", "Message received");
@@ -181,17 +255,12 @@ public class MainChatActivity extends Activity {
 	}
 
 
-	// Find position of JabberID in ArrayList
-	private int getChatIndex(String from) {
-		for(int i = 0; i < chats.size(); i++)
-		{
-			if (chats.get(i).getEmail().equals(from))
-				return i;
-		}
-		return -1;
-	}
-
-
+	
+	/**
+	 * Handles Subscription Requests from other Users. (Un-)Subscribes to other Users, depending on the User Input
+	 * 
+	 * @param packet is the Presence Packet that was sent to us
+	 */
 	private void handlePresencePackets(Packet packet) {
 		Presence presence = (Presence)packet;
 		final String sender = presence.getFrom();
@@ -219,29 +288,18 @@ public class MainChatActivity extends Activity {
 				removeUser(new ChatPartner(sender, chatPartners.getEntry(sender).getName()));
 				break;
 				
+			// TODO Handle Other Presence PacketTypes
+				
 			default:
 				Log.i("Presence Listener", "Not handled!");
 		}
-		
-		
-		/*
-		if (presence.getType() == Presence.Type.subscribe ||
-			presence.getType() == Presence.Type.subscribed)
-		{
-			if (chatPartners.contains(presence.getFrom()))
-			{
-				Presence response = new Presence(Presence.Type.subscribed);
-				response.setTo(presence.getFrom());
-				connection.sendPacket(response);
-			}
-		}
-		*/
 	}
 	
 	
 	
 	/**
 	 * Displays a Toast on the UI Thread
+	 * 
 	 * @param string the Messages which is posted
 	 */
 	private void showToast(final String message) {
@@ -258,6 +316,7 @@ public class MainChatActivity extends Activity {
 	
 	/**
 	 * Displays a Dialog to the User when a Subscription Request is incoming
+	 * 
 	 * @param sender who wanted to subscribe
 	 */
 	private void showSubscriptionDialog(final String sender) {
@@ -301,13 +360,17 @@ public class MainChatActivity extends Activity {
 	}
 
 
-	// Is Called when another Activity returns. Adds chosen User to Roster if he exists
+	
+	/**
+	 *  Is Called when another Activity returns. Adds chosen User to Roster if he exists.
+	 */
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 	    super.onActivityResult(requestCode, resultCode, data);
 	    
 	    // Keep Track of Context
 	    context = this;
+	    currentChatPartner = "";
 	    
 		switch(requestCode) 
 		{
@@ -332,10 +395,33 @@ public class MainChatActivity extends Activity {
 				Log.i("Context Tracker", "Returned to MainChatActivity");
 		}
 	}
+
+
+	
+	/**
+	 * Searches for the index a userID in our Array of ChatPartners
+	 * 
+	 * @param userID is the User we want to search
+	 * @return the index of the userID we wanted to search, or -1 if userID was not found
+	 */
+	private int getChatIndex(String userID) {
+		for(int i = 0; i < chats.size(); i++)
+		{
+			if (chats.get(i).getEmail().equals(userID))
+				return i;
+		}
+		return -1;
+	}
+
 	
 
 	
-	 // Add new ChatPartner to Array and subscribe to Server
+	/**
+	 * Add new ChatPartner to Array and subscribe to Server
+	 * 
+	 * @param email is the userID of the new ChatPartner
+	 * @param name is the name that the new ChatPartner is referred to
+	 */
 	private void addUser(final String email, final String name)
 	{
 		try 
@@ -370,7 +456,12 @@ public class MainChatActivity extends Activity {
 	
 	
 	
-	// Remove user from Array and unsubscribe 
+	/**
+	 * Remove user from Array and unsubscribe
+	 *  
+	 * @param user is the User we want to delete
+	 * @return 
+	 */
 	private boolean removeUser(final ChatPartner user)
 	{
 		runOnUiThread(new Runnable() {
@@ -395,7 +486,12 @@ public class MainChatActivity extends Activity {
 	
 	
 	
-	// Check if User exists on google Talk server
+	/**
+	 * Check if User exists on server
+	 * 
+	 * @param userID is the User we want to search for on the Server
+	 * @return true if User exists on Server
+	 */
 	private boolean userExists(String userID)
 	{
 		try 
@@ -407,17 +503,18 @@ public class MainChatActivity extends Activity {
 			Form searchForm = searchManager.getSearchForm(searchService);
 			Form answerForm = searchForm.createAnswerForm();
 			
+			// Split username@service.com into username*, or else search doesn´t work
 			String usernameWithoutDomain = userID.split("@")[0] + "*";
 			answerForm.setAnswer("user", usernameWithoutDomain);
 			
+			// Get Data from SearchService
 			ReportedData resultData = searchManager.getSearchResults(answerForm, searchService);
 			if (resultData.getRows() != null)
-			{
-				
+			{	
 				Iterator<Row> iter = resultData.getRows();
 				if (!iter.hasNext())
 				{
-					Toast.makeText(this, "User "+userID+" does not exist!", Toast.LENGTH_SHORT).show();
+					Toast.makeText(this, "User " + userID + " does not exist!", Toast.LENGTH_SHORT).show();
 					return false;
 				}
 				else
@@ -459,19 +556,27 @@ public class MainChatActivity extends Activity {
 		}
 		catch (Exception e)
 		{
+			Log.e(NEW_USER_TAG, e.toString());
 			e.printStackTrace();
 			return false;
 		}
 	}
-	
-	
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main_chat, menu);
-		return true;
-	}
 
+	
+	
+	
+	/**
+	 * Implements a Filter that accepts all Messages except for those from the User, we are 
+	 * currently in a ChatRoom with
+	 */
+	private class MyPacketFilter implements PacketFilter 
+	{
+		@Override
+		public boolean accept(Packet packet) 
+		{
+			String from = packet.getFrom().split("/")[0];
+			return (! from.equals(currentChatPartner));
+		}
+	}
 
 }
